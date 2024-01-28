2ef8fee1ebb4163400cdb623236b03ae653a8fb5
NatSantosMart <natsantosmartinez@gmail.com>
2024-01-28 14:29:51 +0100

    Migrations OK

--- database/migrations/2024_01_27_151748_create_comments_table.php
+++ database/migrations/2024_01_27_151748_create_comments_table.php
@@ -9,13 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up()
+   public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
             $table->date('date');
             $table->text('description');
-            $table->unsignedInteger('client_dni');
+            $table->string('client_dni', 20);
 
             $table->foreign('client_dni')
                   ->references('dni')
