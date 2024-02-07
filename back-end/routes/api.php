33827726c0e06b95a1c33eb0c4ecbde175254bb6
NatSantosMart <natsantosmartinez@gmail.com>
2024-02-07 22:34:21 +0100

    Integrar servicio http supplement y clothing

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RatingController; 
 use App\Http\Controllers\PersonController; 
 use App\Http\Controllers\ProductController; 
+use App\Http\Controllers\ClothingController; 
+use App\Http\Controllers\SupplementController; 
 use App\Http\Controllers\CartController; 
 use App\Http\Controllers\FavoriteController; 
 use App\Http\Controllers\OrderController; 
@@ -68,6 +70,22 @@ Route::controller(RatingController::class)->prefix('ratings')->group(function() 
     Route::delete('/{comment_id}', 'destroy'); 
 });
 Route::controller(ProductController::class)->prefix('products')->group(function() {
+    Route::get('/', 'index'); 
+    Route::post('/', 'store'); 
+    Route::post('/{id}', 'update'); 
+    Route::put('/{id}', 'put'); 
+    Route::get('/{id}', 'show'); 
+    Route::delete('/{id}', 'destroy'); 
+});
+Route::controller(ClothingController::class)->prefix('clothes')->group(function() {
+    Route::get('/', 'index'); 
+    Route::post('/', 'store'); 
+    Route::post('/{id}', 'update'); 
+    Route::put('/{id}', 'put'); 
+    Route::get('/{id}', 'show'); 
+    Route::delete('/{id}', 'destroy'); 
+});
+Route::controller(ProductController::class)->prefix('supplements')->group(function() {
     Route::get('/', 'index'); 
     Route::post('/', 'store'); 
     Route::post('/{id}', 'update'); 
--- front-end/src/app/services/product.service.ts
+++ front-end/src/app/services/product.service.ts
@@ -1,64 +1,82 @@
 import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { ApiConfig } from "./ApiConfig"; 
 import { Clothing } from "../models/clothes.model";
 import { Product } from "../models/product.model";
 
 @Injectable()
 export class ProductService{
 
-    clothes: Clothing[] = [
-        {
-            id: 1,
-            name: "Camiseta deportiva",
-            price: 45,
-            url_image: "../../../assets/imgs/61823BwBHaL.jpg",
-            description: "Descripcion producto 1",
-            gendre: "Mujer",
-            color: "negro",
-            size: "S"
-        },
-        {
-            id: 2,
-            name: "Camiseta deportiva",
-            price: 50,
-            url_image: "../../../assets/imgs/1179300812_2_1_5.jpg",
-            description: "Descripcion producto 2",
-            gendre: "Mujer",
-            color: "negro",
-            size: "M"  
-        },
-        {
-            id: 3,
-            name: "Camiseta deportiva",
-            price: 55,
-            url_image: "../../../assets/imgs/3-2.jpg",
-            description: "Descripcion producto 3",
-            gendre: "Mujer",
-            color: "negro",
-            size: "S"
-        }
-    ]                      
+    
+    constructor(
+        private http: HttpClient
+    ){}
 
-    getAllClothes() : Clothing[]{
-        return this.clothes;
+    getAllProducts(): Observable<any>{
+        return this.http.get(`${ApiConfig.baseUrl}/products`);
     }
 
-    getProductById(id : number) : Clothing | undefined {
+    createProduct(newProduct: Product): Observable<any> {
+        let params = JSON.stringify(newProduct); 
+        let headersCreate = new HttpHeaders().set('Content-Type', 'application/json'); 
+        return this.http.post(`${ApiConfig.baseUrl}/products`, params, {headers:headersCreate});
+    }
+
+    // clothes: Clothing[] = [
+    //     {
+    //         id: 1,
+    //         name: "Camiseta deportiva",
+    //         price: 45,
+    //         url_image: "../../../assets/imgs/61823BwBHaL.jpg",
+    //         description: "Descripcion producto 1",
+    //         gendre: "Mujer",
+    //         color: "negro",
+    //         size: "S"
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Camiseta deportiva",
+    //         price: 50,
+    //         url_image: "../../../assets/imgs/1179300812_2_1_5.jpg",
+    //         description: "Descripcion producto 2",
+    //         gendre: "Mujer",
+    //         color: "negro",
+    //         size: "M"  
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "Camiseta deportiva",
+    //         price: 55,
+    //         url_image: "../../../assets/imgs/3-2.jpg",
+    //         description: "Descripcion producto 3",
+    //         gendre: "Mujer",
+    //         color: "negro",
+    //         size: "S"
+    //     }
+    // ]                      
+
+    // getAllClothes() : Clothing[]{
+    //     return this.clothes;
+    // }
+
+    // getProductById(id : number) : Clothing | undefined {
         
-        return this.clothes.find(clothing => clothing.id == id)
-    }
+    //     return this.clothes.find(clothing => clothing.id == id)
+    // }
 
-    deleteProduct(id : number) : boolean{
-        console.log(id)
-        return true;
-    }
+    // deleteProduct(id : number) : boolean{
+    //     console.log(id)
+    //     return true;
+    // }
 
-    addProduct(product : Product) : boolean{
-        console.log(product)
-        return true;
-    }
+    // addProduct(product : Product) : boolean{
+    //     console.log(product)
+    //     return true;
+    // }
 
-    editProduct(product : Product) : boolean{
-        console.log(product)
-        return true;
-    }
+    // editProduct(product : Product) : boolean{
+    //     console.log(product)
+    //     return true;
+    // }
 }
