bfff51f6f0c678ea80689246c4291f6f32575564
NatSantosMart <natsantosmartinez@gmail.com>
2024-02-11 14:35:30 +0100

    datos prueba ddbb

--- back-end/database/seeders/ClothingSeeder.php
+++ back-end/database/seeders/ClothingSeeder.php
@@ -54,11 +54,55 @@ class ClothingSeeder extends Seeder
                 'product_id' => 11,
             ],
             [
-                'gender' => 'Masculino',
-                'size' => 'XL',
-                'color' => 'Rojo',
-                'product_id' => 12,
+                'gender' => 'Femenino',
+                'size' => 'L',
+                'color' => 'Blanco',
+                'product_id' => 13,
             ],
+            [
+                'gender' => 'Femenino',
+                'size' => 'S',
+                'color' => 'Negro',
+                'product_id' => 14,
+            ],
+            [
+                'gender' => 'Femenino',
+                'size' => 'M',
+                'color' => 'Negro',
+                'product_id' => 15,
+            ],
+            [
+                'gender' => 'Masculino',
+                'size' => 'L',
+                'color' => 'Negro',
+                'product_id' => 16,
+            ],
+
+            [
+                'gender' => 'Masculino',
+                'size' => 'S',
+                'color' => 'Rojo',
+                'product_id' => 17,
+            ],
+            [
+                'gender' => 'Masculino',
+                'size' => 'M',
+                'color' => 'Rojo',
+                'product_id' => 18,
+            ],
+            [
+                'gender' => 'Masculino',
+                'size' => 'L',
+                'color' => 'Negro',
+                'product_id' => 19,
+            ],
+            [
+                'gender' => 'Femenino',
+                'size' => 'M',
+                'color' => 'Rojo',
+                'product_id' => 20,
+            ],
+            
         ];
 
         DB::table('clothing')->insert($clothing);
